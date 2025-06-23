6cf0f6cf89f9575bf9d6ad3beff7130d2893f11e
jibaek <tngus000777@gmail.com>
2025-06-23 15:33:53 +0900

    사용자 관리 및 연관관계 설정

--- src/main/java/com/tenco/blog/board/Board.java
+++ src/main/java/com/tenco/blog/board/Board.java
@@ -1,5 +1,6 @@
 package com.tenco.blog.board;
 
+import com.tenco.blog.user.User;
 import com.tenco.blog.utils.MyDateUtil;
 import jakarta.persistence.*;
 import lombok.Data;
@@ -21,23 +22,24 @@ public class Board {
     private String title;
     private String content;
     private String username;
+    // V2에서 사용했던 방식
+    // private String username;
+    // V3 에서 Board 엔티티는 User 엔티티와 연관관계가 성립이 된다.
 
-    // CreationTimestamp : 하이버네이트가 제공하는 어노테이션
-    // 엔티티가 처음 저장할 때 현재 시간을 자동으로 설정한다.
-    // pc -> db(날짜 주입)
-    // v1에서는 SQL now()를 직접 사용했지만 JPA가 자동 처리
+    // 다대일
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")// 외래키 컬러명 명시
+    private User user;
+
     @CreationTimestamp
     private Timestamp createdAt; // created_at () (스네이크 케이스로 자동 변환)
 
-    // 생성자 만들어 주기
-    public Board(String title, String content,String username) {
-        this.title = title;
-        this.content = content;
-        this.username = username;
-        // id와 createAt 은 JPA/Hibernate 가 자동으로 설정
-    }
+//    public Board(String title, String content,String username) {
+//        this.title = title;
+//        this.content = content;
+//        this.username = username;
+//    }
 
-    // 머스태치에서 표현할 시간을 포맷기능을(행위) 스스로 만들자
     public String getTime() {
         return MyDateUtil.timestampFormat(createdAt);
     }
